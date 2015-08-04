dff14f86b1a7d81a33c4ad45dfa73a2c3f109d22
Aaron Decker <aaron@ard.ninja>
2015-08-04 08:31:36 -0400

    cleanup readme

--- README.md
+++ README.md
@@ -6,16 +6,16 @@ A fun way to annoy your deadbeat roommate who hasn't paid the rent. Programatica
 ### Usage:
 
 ```
-	node ./app.js --config='./config-example.js' --message='Pay the rent bro!'  --target='xxxxxxxxxxx' --type='sms'
+	node ./app.js --config='./config-example.js' --message='Pay the rent bro!'  --target='18001234567' --type='sms'
 ```
 
-I provided some examples of how to do this but essentially you will set your Plivo account up, create a config file, and run the app like in the example above. I even added a crob job example to show you how to be extra annoying.
+I provided some examples of how to do this but essentially you will set your Plivo account up, create a config file, and run the app like in the example above. I even added a crob job example to show you how to be extra annoying. Make sure to use the country code in from of the phone number like in the example. 
 
 
 
 ### SMS integration
 
-I'm using plivo because it is cheaper than twilio. SMS is pretty simple, you supply the 'target' phone number in the command line argument as well as the message and it will send immediately. 
+I'm using plivo because it is cheaper than twilio. SMS is pretty simple, you supply the 'target' phone number in the command line argument as well as the message and it will send immediately. Type 'sms' is optional as it is the default.
 
 
 ### Voice integration
@@ -23,6 +23,13 @@ I'm using plivo because it is cheaper than twilio. SMS is pretty simple, you sup
 So with the Plivo voice api you can create the call from the application, but the Plivo API will then hit some URL when the call is picked up, so you must supply this URL when you create the call. Plivo expects you to have some XML at this endpoint that you have specified. The XML is pretty simple, and the parameter where you set the endpoint is "answer_url" in the config file. You can just put your XML file up on S3 or even link it to this repository, it doesn't really matter.
 
 
+Example voice command:
+```
+	node ./app.js --config='./config-example.js'  --target='18001234567' --type='voice'
+```
+
+
+The structure of the file at your answer_url:
 ```
 <Response>
     <Speak language="en-GB" loop="3" voice="WOMAN">
--- app.js
+++ app.js
@@ -11,11 +11,6 @@ if(!argv.config) {
 }
 config = require(argv.config);
 
-if(!argv.message) {
-    throw new Error('No message found in arguments.');
-}
-message = argv.message;
-
 if(!argv.target) {
     throw new Error('No target phone found in arguments.');
 }
@@ -24,30 +19,42 @@ target = argv.target;
 if(argv.type && ['sms', 'voice'].indexOf(argv.type) === -1) {
     throw new Error('No valid type found in arguments, must be "sms" or "voice".');
 } else if(argv.type) {
-	type = argv.type;
+    type = argv.type;
 } else {
-	type = 'sms'; // default
+    type = 'sms'; // default
 }
 
 if(type === 'sms') {
-	var plivoSMS = new PlivoSMS(config);
-	plivoSMS.send(target, message, function(error, results){
-	    if(error) {
-	        console.error('Error: ', error);
-	    } else {
-	        console.log('Message sent: ', results);
-	    }
-	});
+    // message is only requried for SMS mode.
+    if(!argv.message) {
+        throw new Error('No message found in arguments.');
+    }
+    message = argv.message;
+
+    var plivoSMS = new PlivoSMS(config);
+    plivoSMS.send(target, message, function(error, results) {
+        if(error) {
+            console.error('Error: ', error);
+        } else {
+            console.log('Message sent: ', results);
+        }
+    });
 } else if(type === 'voice') {
-	var plivoVoice = new PlivoVoice(config);
-	plivoVoice.send(target, message, function(error, results){
-	    if(error) {
-	        console.error('Error: ', error);
-	    } else {
-	        console.log('Message sent: ', results);
-	    }
-	});
+    // In voice mode, we do not use 'message', we use the XML found at 'answer_url',
+    // sorry that is just how Plivo (and Twillio for that matter) happens to work.
+    if(!config.plivo.answer_url) {
+        throw new Error('Config element "plivo.answer_url" is required for voice mode');
+    }
+
+    var plivoVoice = new PlivoVoice(config);
+    plivoVoice.send(target, message, function(error, results) {
+        if(error) {
+            console.error('Error: ', error);
+        } else {
+            console.log('Message sent: ', results);
+        }
+    });
 } else {
-	throw new Error('Unknown type selected');
+    throw new Error('Unknown type selected');
 }
 
